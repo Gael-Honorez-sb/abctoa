6c4c97740626980ee32b24aa970e5e34db2e423c
gael-honorez-sb <gael@starbreeze.com>
2014-09-10 15:28:19 +0200

    Alembic Holder to LGPL.

--- maya/alembicHolder/nozAlembicHolderNode.h
+++ maya/alembicHolder/nozAlembicHolderNode.h
@@ -1,14 +1,20 @@
+/*Alembic Holder
+Copyright (c) 2014, Gaël Honorez, All rights reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public
+License along with this library.*/
+
+
+
 #ifndef _nozAlembicHolderNode
 #define _nozAlembicHolderNode
-//
-// Copyright (C) 2014 Nozon.
-//
-// File: nozAlembicHolderNode.h
-//
-// Dependency Graph Node: nozAlembicHolder
-//
-// Author: Gaël Honorez
-//
 
 #include "Foundation.h"
 #include "Transport.h"
