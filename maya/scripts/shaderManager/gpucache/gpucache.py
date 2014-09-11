98b9337dfdf10c276b31f9f37150a12fd025ebdd
gael-honorez-sb <gael@starbreeze.com>
2014-09-11 17:24:46 +0200

    Updating license.

--- maya/scripts/shaderManager/gpucache/gpucache.py
+++ maya/scripts/shaderManager/gpucache/gpucache.py
@@ -1,20 +1,15 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2014 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
+# Alembic Holder
+# Copyright (c) 2014, Gaël Honorez, All rights reserved.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.
 
 from shaderManager.assignations._assignations import cacheAssignations
 
--- maya/scripts/shaderManager/ui/UI_ABCHierarchy.py
+++ maya/scripts/shaderManager/ui/UI_ABCHierarchy.py
@@ -1,5 +1,17 @@
-# -*- coding: utf-8 -*-
 
+# -*- coding: utf-8 -*-
+# Alembic Holder
+# Copyright (c) 2014, Gaël Honorez, All rights reserved.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.
 # Form implementation generated from reading ui file 'UI_ABCHierarchy.ui'
 #
 # Created: Thu Sep 11 16:49:59 2014
