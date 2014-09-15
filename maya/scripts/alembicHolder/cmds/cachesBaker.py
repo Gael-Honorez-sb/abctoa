0e1774cf4a731ca12d910b535a434e36a520281f
gael-honorez-sb <gael@starbreeze.com>
2014-09-15 17:50:00 +0200

    Import & Export cache assignations.

--- maya/scripts/alembicHolder/cmds/cachesBaker.py
+++ maya/scripts/alembicHolder/cmds/cachesBaker.py
@@ -13,69 +13,135 @@
 
 import maya.cmds as cmds
 import json
+import os
+
+def getHolder(x):
+    ''' return the shape of an holder from the shape or the group'''
+
+    shape = None
+    if cmds.nodeType(x) == "alembicHolder":
+        shape = x
+    else:
+        shapes = cmds.listRelatives(x, shapes=True)
+        if shapes:
+            shape = shapes[0]
+    return shape
+
+
+def applyAssignations():
+    ''' select a json file, and apply it to a alembicHolder '''
+    
+    tr = cmds.ls( type= 'transform', sl=1) + cmds.ls(type= 'alembicHolder', sl=1)
+    
+    if len(tr) == 0:
+        return
+            
+    singleFilter = "Json Files (*.json)"
+    jsonfiles = cmds.fileDialog2(fileMode=1, caption="Import Json", fileFilter=singleFilter)
+    if len(jsonfiles) > 0:
+        jsonfile = jsonfiles[0]
+    else:
+        return
+
+    if os.path.exists(jsonfile):
+        shaderFile = None
+        potentialFile = os.path.join(os.path.dirname(jsonfile), "shaders_%s" % os.path.basename(jsonfile)).replace(".json", ".abc")
+        if os.path.exists(potentialFile):
+            shaderFile = potentialFile
+
+        for x in tr:
+            shape = getHolder(x)
+            if not shape:
+                continue
+
+            cmds.setAttr("%s.jsonFile" % shape, jsonfile, type="string")
+            if shaderFile:
+                cmds.setAttr("%s.abcShaders" % shape, shaderFile, type="string")
+
+
+def writeAbcShaders():
+    ''' export all shaders connected to a holder in abc '''
+
+    cmds.loadPlugin('abcMayaShader.mll', qt=1)
+
+    tr = cmds.ls( type= 'transform', sl=1) + cmds.ls(type= 'alembicHolder', sl=1)
+    
+    if len(tr) == 0:
+        return
+
+    for x in tr:
+        shape = getHolder(x)
+        if not shape:
+            continue
+
+        cache = cmds.getAttr("%s.cacheFileName" % shape)
+        abcfile = os.path.join(os.path.dirname(cache), "shaders_%s" % os.path.basename(cache))
+        try:
+            cmds.abcCacheExport(f=abcfile, node=shape)
+        except Exception, e:
+            print "Failed to export Abc Shaders: %s" % e
 
 def writeJson(namespace=None):
-	'''Write a json file next to the selected alembicHolders cache containing the current assignations.
+    '''Write a json file next to the selected alembicHolders cache containing the current assignations.
 
-	Keyword arguments:
-	namespace -- When looking for shaders in the procedural, look under this namespace.
-	'''
+    Keyword arguments:
+    namespace -- When looking for shaders in the procedural, look/use this namespace.
+    '''
 
-	tr = cmds.ls( type= 'transform', sl=1) + cmds.ls(type= 'alembicHolder', sl=1)
-	
-	if len(tr) == 0:
-	    return
-	
-	for x in tr:
-	    if cmds.nodeType(x) == "alembicHolder":
-	        shape = x
-	    else:
-	        shapes = cmds.listRelatives(x, shapes=True)
-	        if shapes:
-	            shape = shapes[0]
+    tr = cmds.ls( type= 'transform', sl=1) + cmds.ls(type= 'alembicHolder', sl=1)
 
-	    if cmds.nodeType(shape) == "gpuCache" or cmds.nodeType(shape) == "alembicHolder":
+    if len(tr) == 0:
+        return
+    
+    for x in tr:
+        shape = getHolder(x)
+        if not shape:
+        	continue
 
-	    	cache = cmds.getAttr("%s.cacheFileName" % shape)
-	    	jsonfile = cache.replace(".abc", ".json")
+        if cmds.nodeType(shape) == "gpuCache" or cmds.nodeType(shape) == "alembicHolder":
 
-	    	assignations = {}
+            cache = cmds.getAttr("%s.cacheFileName" % shape)
+            jsonfile = cache.replace(".abc", ".json")
 
-	    	if cmds.objExists("%s.mtoa_constant_shaderAssignation" % shape):
-	    		try:
-	    			cur = cmds.getAttr("%s.mtoa_constant_shaderAssignation"  % shape)
-	    			assignations["shaders"] = json.loads(cur)
-	    		except:
-	    			pass
+            assignations = {}
 
-	    	if cmds.objExists("%s.mtoa_constant_overrides" % shape):
-	    		try:
-		    		cur = cmds.getAttr("%s.mtoa_constant_overrides"  % shape)
-		    		assignations["overrides"] = json.loads(cur)
-	    		except:
-	    			pass
+            if cmds.objExists("%s.shadersAssignation" % shape):
+                try:
+                    cur = cmds.getAttr("%s.shadersAssignation"  % shape)
+                    assignations["shaders"] = json.loads(cur)
+                except:
+                    pass
 
-	    	if cmds.objExists("%s.mtoa_constant_displacementAssignation" % shape):
-	    		try:
-		    		cur = cmds.getAttr("%s.mtoa_constant_displacementAssignation"  % shape)
-		    		assignations["displacement"] = json.loads(cur)
-	    		except:
-	    			pass
+            if cmds.objExists("%s.attributes" % shape):
+                try:
+                    cur = cmds.getAttr("%s.attributes"  % shape)
+                    assignations["attributes"] = json.loads(cur)
+                except:
+                    pass
 
-	    	if cmds.objExists("%s.mtoa_constant_layerOverrides" % shape):
-	    		try:
-		    		cur = cmds.getAttr("%s.mtoa_constant_layerOverrides"  % shape)
-		    		assignations["layers"] = json.loads(cur)
-	    		except:
-	    			pass
+            if cmds.objExists("%s.displacementsAssignation" % shape):
+                try:
+                    cur = cmds.getAttr("%s.displacementsAssignation"  % shape)
+                    assignations["displacement"] = json.loads(cur)
+                except:
+                    pass
 
-	    	if namespace:
-	    		assignations["namespace"] = namespace
+            if cmds.objExists("%s.layersOverride" % shape):
+                try:
+                    cur = cmds.getAttr("%s.layersOverride"  % shape)
+                    assignations["layers"] = json.loads(cur)
+                except:
+                    pass
 
-	    	try:
-	    		with open(jsonfile, 'w') as outfile:
-		    		json.dump(assignations, outfile, separators=(',',':'), sort_keys=True, indent=4))
-			except:
-				print "Error writing json file", outfile
-	    		
-	    		
+            if namespace:
+                assignations["namespace"] = namespace
+            else :
+                assignations["namespace"] = shape.replace(":", "_").replace("|", "_")
+
+            try:
+                with open(jsonfile, 'w') as outfile:
+                    json.dump(assignations, outfile, separators=(',',':'), sort_keys=True, indent=4)
+            except:
+                print "Error writing json file", outfile
+                
+                
--- maya/scripts/alembicHolder/cmds/registerAlembicHolder.py
+++ maya/scripts/alembicHolder/cmds/registerAlembicHolder.py
@@ -26,9 +26,24 @@ def createAlembicHolder():
     cmds.setAttr("%s.visibleInReflections" % x, 1)
     cmds.connectAttr("time1.outTime", "%s.time" % x)
 
+def exportAssignations():
+    ''' export json and shaders'''
+    from alembicHolder.cmds import cachesBaker
+    cachesBaker.writeAbcShaders()
+    cachesBaker.writeJson()
+
+def importAssignations():
+    ''' select a json file, and apply it to a alembicHolder '''
+    from alembicHolder.cmds import cachesBaker
+    cachesBaker.applyAssignations()
+
+
 
 def registerAlembicHolder():
     if not cmds.about(b=1):
         cmds.menu('AlembicHolderMenu', label='Alembic Holder', parent='MayaWindow', tearOff=True )
         cmds.menuItem('CreateAlembicHolder', label='Create Holder', parent='AlembicHolderMenu', c=lambda *args: createAlembicHolder())
         cmds.menuItem('AlembicShaderManager', label='Shader Manager', parent='AlembicHolderMenu', c=lambda *args: alembicShaderManager())
+        cmds.menuItem( divider=True )
+        cmds.menuItem('exportAssign', label='Export Assignations on selected caches', parent='AlembicHolderMenu', c=lambda *args: exportAssignations())
+        cmds.menuItem('importtAssign', label='Import Assignation on selected caches', parent='AlembicHolderMenu', c=lambda *args: importAssignations())
