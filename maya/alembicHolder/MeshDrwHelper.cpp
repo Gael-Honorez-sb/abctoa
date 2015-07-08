cfac56b481243651a49f489cf2ef85d50c753dfc
gael-honorez-sb <gael@starbreeze.com>
2015-07-08 18:04:20 +0200

    reverted bad change.

--- maya/alembicHolder/MeshDrwHelper.cpp
+++ maya/alembicHolder/MeshDrwHelper.cpp
@@ -53,7 +53,6 @@ MeshDrwHelper::MeshDrwHelper()
 MeshDrwHelper::~MeshDrwHelper()
 {
     makeInvalid();
-	buffer.clear();
 }
 
 //-*****************************************************************************
