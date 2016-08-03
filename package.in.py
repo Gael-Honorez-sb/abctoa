493a38c717f16458670ec3764db7d2358cfaa5b2
gael-honorez-sb <gael@starbreeze.com>
2016-08-03 15:13:41 +0200

    fixing package script.

--- package.in.py
+++ package.in.py
@@ -31,10 +31,12 @@ name_osx = 'abcToA-osx-%s-maya-%s-MtoA-%s' % (ABCTOA_VERSION, MAYA_VERSION, MTOA
 name_win = 'abcToA-win-%s-maya-%s-MtoA-%s' % (ABCTOA_VERSION, MAYA_VERSION, MTOA_VERSION)
 name_linux = 'abcToA-linux-%s-maya-%s-MtoA-%s' % (ABCTOA_VERSION, MAYA_VERSION, MTOA_VERSION)
 
+
 def copyFilesToDistDir(files, distDir):
     for fn in files:
         shutil.copy(fn, distDir)
-        
+
+
 def createArchive(distDir, name, isSrc=False):
     if not isSrc and platform.system() == "Windows":
         f = zipfile.ZipFile(os.path.join('..', '%s.zip' % name), 'w')
@@ -43,53 +45,56 @@ def createArchive(distDir, name, isSrc=False):
         f.close()
     else:
         f = tarfile.open(os.path.join('..', '%s.tar.gz' % name), 'w:gz')
-        f.add(distDir, arcname = name)
+        f.add(distDir, arcname=name)
         f.close()
-    
+
+
 def createBinaryDistribution(name, droot):
+    outname = ""
     if platform.system() == "Windows":
-	print "writing", os.path.join('%s.zip' % name)
+        outname = os.path.join('%s.zip' % name)
+        print "writing", outname
         zf = zipfile.ZipFile(os.path.join('%s.zip' % name), 'w')
         for root, dirs, files in os.walk(droot):
             rroot = os.path.relpath(root, droot)
             for f in files:
                 if not f.endswith(".lib"):
                     rfile = os.path.join(rroot, f)
-                    zf.write(os.path.join(droot, rfile), arcname=os.path.join("modules", "abcToA-%s" % ABCTOA_VERSION,rfile))
+                    zf.write(os.path.join(droot, rfile), arcname=os.path.join("modules", "abcToA-%s" % ABCTOA_VERSION, rfile))
         for f in files_src:
             zf.write(f, arcname=os.path.join(f))
         zf.write("abcToA.mod", arcname=os.path.join("modules", "abcToA.mod"))
     else:
-	print "writing", os.path.join('%s.tar.gz' % name)
+        outname = os.path.join('%s.tar.gz' % name)
+        print "writing", outname
         zf = tarfile.open(os.path.join('%s.tar.gz' % name), 'w:gz')
-	for root, dirs, files in os.walk(droot):
+        for root, dirs, files in os.walk(droot):
             rroot = os.path.relpath(root, droot)
             for f in files:
-            	rfile = os.path.join(rroot, f)
-	        zf.add(os.path.join(droot, rfile), arcname=os.path.join("modules", "abcToA-%s" % ABCTOA_VERSION,rfile))
-
+                rfile = os.path.join(rroot, f)
+                zf.add(os.path.join(droot, rfile), arcname=os.path.join("modules", "abcToA-%s" % ABCTOA_VERSION, rfile))
         for f in files_src:
             zf.add(f, arcname=os.path.join(f))
-	zf.add("abcToA.mod", arcname=os.path.join("modules", "abcToA.mod"))
+        zf.add("abcToA.mod", arcname=os.path.join("modules", "abcToA.mod"))
         zf.close()
+
+    return outname
 
 # Binary distribution
 droot = '%s' % ("@INSTALL_DIR@")
 
 if platform.system() == "Darwin":
     # OS X distribution
-    createBinaryDistribution(name_osx, droot)
+    outname = createBinaryDistribution(name_osx, droot)
 elif platform.system() == "Windows":
     # Windows
-    createBinaryDistribution(name_win, droot)
+    outname = createBinaryDistribution(name_win, droot)
     serverPath = "//server01/shared/Dev/AbcToA_Builds"
     if os.path.exists(serverPath):
-        shutil.copyfile(os.path.join('%s.zip' % name_win), serverPath)
+        shutil.copyfile(outname, os.path.join(serverPath, outname))
 
 elif platform.system() == "Linux":
     # Linux
-    createBinaryDistribution(name_linux, droot)
+    outname = createBinaryDistribution(name_linux, droot)
 else:
     print 'Warning: unknown system "%s", not creating binary package' % platform.system()
-
-    
