c8eaa1585e60af62c1a61bd233ad240258fe0d5e
gael-honorez-sb <gael@starbreeze.com>
2016-08-18 17:05:52 +0200

    Reverting CMakefiles.

--- share/FindPyString.cmake
+++ share/FindPyString.cmake
@@ -1,5 +1,55 @@
-find_library(PYSTRING_LIB NAMES pystring PATHS ${PYSTRING_LIBRARY} NO_DEFAULT_PATH)
-SET( PYSTRING_LIBS ${PYSTRING_LIB} )
 
-MESSAGE(STATUS "PYSTRING INCLUDE PATH: ${PYSTRING_INCLUDE_DIR}" )
-MESSAGE(STATUS "PYSTRING LIBRARIES: ${PYSTRING_LIBS}" )
+# The module defines the following variables:
+#	PYSTRING_LIBRARY_DIR - path to PYstring static libs
+#       PYSTRING_FOUND 	    - true if the Alembic was found
+#
+# Example usage:
+#   find_package(PYSTRING)
+#   if(PYSTRING_FOUND)
+#     message("Pystring found: ${PYSTRING_LIBRARY_DIR}")
+#   endif()
+#
+#=============================================================================
+
+message("-- Searching PyString libraries.......")
+
+IF(NOT PYSTRING_DIR AND NOT $ENV{PYSTRING_DIR} STREQUAL "")
+  SET(PYSTRING_DIR $ENV{PYSTRING_DIR})
+ENDIF()
+
+
+set(LIBRARY_PATHS 
+	/usr/lib
+	/usr/local/lib
+	/sw/lib
+	/opt/local/lib
+	${PYSTRING_DIR}/lib/
+	${PYSTRING_DIR}/lib/static)
+
+# Find PYstring lib
+
+find_library(PYSTRING_LIBRARY
+	NAMES pystring libpystring
+	PATHS ${LIBRARY_PATHS})		
+
+set ( PYSTRING_LIBRARIES
+	${PYSTRING_LIBRARY}
+	)
+
+get_filename_component( PYSTRING_LIBRARY_DIR ${PYSTRING_LIBRARY} PATH )
+
+find_path ( PYSTRING_INCLUDE_DIR pystring.h
+           ${PYSTRING_DIR}/include
+          )
+		  
+include( FindPackageHandleStandardArgs )
+
+find_package_handle_standard_args( "PYSTRING" DEFAULT_MSG
+				PYSTRING_LIBRARIES
+                PYSTRING_LIBRARY_DIR	
+				PYSTRING_INCLUDE_DIR				   
+				  )
+if(NOT PYSTRING_FOUND)
+    message(FATAL_ERROR "Try using -D PYSTRING_DIR=/path/to/pystring")
+endif()								
+	
